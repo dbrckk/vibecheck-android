23cd571aaea0627c55840291df1b656802f90f10
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-23 16:18:17 +0200

    copy: make challenge sharing semantically accurate

--- app/src/main/java/com/vibecheck/app/sharing/ChallengeSharer.kt
+++ app/src/main/java/com/vibecheck/app/sharing/ChallengeSharer.kt
@@ -8,7 +8,7 @@ import com.vibecheck.app.domain.ChallengeLinkCodec
 object ChallengeSharer {
     fun share(context: Context, challenge: Challenge) {
         val link = ChallengeLinkCodec.encode(challenge)
-        val text = "Je te défie sur VibeCheck 😈 Bats mon score de ${challenge.targetPercent}% : $link"
+        val text = "Je te défie sur VibeCheck 😈 Notre groupe a atteint ${challenge.targetPercent}% de consensus. À vous de faire mieux : $link"
 
         val intent = Intent(Intent.ACTION_SEND).apply {
             type = "text/plain"
