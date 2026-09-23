997f8540df11d5d632aff390778979b111bbb005
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-23 12:43:30 +0200

    build: add Play Billing 9.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.11.0")
+    implementation("com.android.billingclient:billing-ktx:9.1.0")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.ui:ui")
