f6d78b093595100dc8c6a0f153b03201f539da94
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-23 11:57:38 +0200

    build: add explicit AndroidX core dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
     implementation(composeBom)
     androidTestImplementation(composeBom)
     implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.ui:ui")
