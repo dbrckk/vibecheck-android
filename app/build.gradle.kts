34f41450f7ae2ebb50cd85db015d56e8f86190d7
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-26 17:58:18 +0200

    build: bump app version to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.dbrckk.vibecheck"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
     sourceSets.getByName("main").res.srcDir(generatedAmbientRes)
     buildFeatures { compose = true }
